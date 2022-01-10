5416d47db8419cd6b5569cd747d0c2b64fd227d4
Eyrum <wocaobukeneng@gmail.com>
2022-01-10 17:09:27 +0800

    c13

---
+++ c13/project/13.6.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+#define NUM_PLANETS 9
+
+char *ToUp( char* );
+
+int main(int argc, char *argv[])
+{
+  char *planets[] = {"Mercury",
+                     "Venus",
+                     "Earth",
+                     "Mars",
+                     "Jupiter",
+                     "Saturn",
+                     "Uranus",
+                     "Neptune",
+                     "Pluto"};
+
+  int i, j;
+
+  for (i = 1; i < argc; i++)
+  {
+    for (j = 0; j < NUM_PLANETS; j++)
+    {
+      if (strcmp(ToUp(argv[i]), ToUp(planets[j])) == 0)
+      {
+        printf("%s is planet %d\n", argv[i], j + 1);
+        break;
+      }
+    }
+    if (j == NUM_PLANETS)
+    {
+      printf("%s is not a planet\n", argv[i]);
+    }
+  }
+
+  return 0;
+}
+
+char *ToUp(char *p)
+{
+  while (*p)
+  {
+    *p = toupper(*p);
+    p++;
+  }
+  return p;
+}
